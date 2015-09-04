6089c0e6d2050c2b7147fbbf401776b6c2136a16
unknown <gabydevon@hotmail.com>
2015-09-04 11:43:46 -0500

    actividades prueba

---
+++ app/src/main/java/com/androiddv/vin_15/comunicaactividades/HermanitoActividad2.java
@@ -0,0 +1,22 @@
+package com.androiddv.vin_15.comunicaactividades;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HermanitoActividad2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hermanito_actividad2);
+        String mensajito = getIntent().getStringExtra("SendwL");
+        Log.d("Hola a todos soy una act", mensajito);
+
+    }
+
+
+}
